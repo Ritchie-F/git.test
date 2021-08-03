15318f4b202950beba38a092ecaa0f69dfe0de0f
Ritchie <15651628119@163.com>
2021-08-03 16:10:02 +0000

    添加gite test

--- src/main/java/com/Ritchie/git/GitTest.java
+++ src/main/java/com/Ritchie/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hot-fix test2");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("Gitee test");
     }
 }
